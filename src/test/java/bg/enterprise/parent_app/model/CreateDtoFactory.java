24d801c7c61057608eb042668981ab4c059a43a0
Damageddream <marcan.grab@gmail.com>
2025-04-04 17:02:59 +0200

    feature/integration-test: test

--- src/test/java/bg/enterprise/parent_app/model/CreateDtoFactory.java
+++ src/test/java/bg/enterprise/parent_app/model/CreateDtoFactory.java
@@ -33,6 +33,7 @@ public class CreateDtoFactory {
                 .prescriptions(prescriptionDtoList)
                 .notifications(notificationDtoList)
                 .build();
+        //tesdt
     }
 
 }
